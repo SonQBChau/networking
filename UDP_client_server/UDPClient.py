f7dfcb39e0bcb089c9cb27a4d635931bcd7092c3
Son Chau <soncqb@gmail.com>
2021-02-07 23:41:17 -0600

    worked

--- UDP_client_server/UDPClient.py
+++ UDP_client_server/UDPClient.py
@@ -1,34 +1,61 @@
-
+#############################################
+# Name: Son Chau (sonchau@my.unt.edu)
+# Course: CSE 5580 Networking Spring 2021
+# Date: 02/07/2021
+# Description: Client UDP program to send and
+# receive packet, calculate round trip time,
+# assume packet loss in 1 second and print
+# the summary status
+#############################################
+import argparse
 import socket
+import timeit
+from statistics import mean
 
- 
+def main(host_name, port_number):
+    client_msg = "PING"
+    bytes_to_send = str.encode(client_msg)
+    buffer_size = 1024
+    trials = 10
+    no_msg_received = 0
+    no_msg_loss = 0
+    rtt_list = []
 
-msgFromClient       = "Hello UDP Server"
+    # Create a UDP socket at client side
+    client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # time out set to 1 second
+    client_socket.settimeout(1)
 
-bytesToSend         = str.encode(msgFromClient)
+    for trial in range (0, trials):
+        print("{}: Sent...".format(trial+1), end =" ")
+        try:
+            start = timeit.default_timer()
+            # Send to server using created UDP socket
+            client_socket.sendto(bytes_to_send, ('127.0.0.1', int(port_number)))
+            msg_from_server = client_socket.recvfrom(buffer_size)
+            stop = timeit.default_timer()
+            rtt = (stop - start) * 1000
+            print("RTT={:.6f} ms".format(rtt))
+            rtt_list.append(rtt)
+            no_msg_received += 1
 
-serverAddressPort   = ("127.0.0.1", 20001)
+            
+        except:
+            print("Request time out!")
+            no_msg_loss += 1
+    
+    print("The number of messages sent: {}".format(trials))
+    print("The number of messages received: {}".format(no_msg_received))
+    print("The message loss rate: {:.0%}".format(no_msg_loss/trials))
+    print("The maximum RRT: {:.6f} ms".format(max(rtt_list)))
+    print("The minimum RRT: {:.6f} ms".format(min(rtt_list)))
+    print("The average RRT: {:.6f} ms".format(mean(rtt_list)))
 
-bufferSize          = 1024
 
- 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("HOST_NAME", help="Host name")
+    parser.add_argument("PORT_NUMBER", help="Port number")
+    args = parser.parse_args()
 
-# Create a UDP socket at client side
-
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
- 
-
-# Send to server using created UDP socket
-
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-
- 
-
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
- 
-
-msg = "Message from Server {}".format(msgFromServer[0])
-
-print(msg)
+    main(args.HOST_NAME, args.PORT_NUMBER)
--- UDP_client_server/UDPServer.py
+++ UDP_client_server/UDPServer.py
@@ -1,47 +1,35 @@
+import argparse
+import random
 import socket
 
+def main(port_number):
+    buffer_size = 1024
+    msg_from_server = "PONG"
+    bytes_to_send = str.encode(msg_from_server)
 
-localIP = "127.0.0.1"
+    # Create a datagram socket
+    server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-localPort = 20001
+    # Bind to address and ip
+    server_socket.bind(('127.0.0.1', int(port_number)))
+    print("[server]: ready to accept data...")
 
-bufferSize = 1024
+    # Listen for incoming datagrams
+    while(True):
+        bytesAddressPair = server_socket.recvfrom(buffer_size)
+        message = bytesAddressPair[0]
+        address = bytesAddressPair[1]
 
+        if random.random() < .3: # simulate 30% packet loss
+            print("[server]: dropped packet")
+        else:# Sending a reply to client
+            print("[client]: {}".format(message))
+            server_socket.sendto(bytes_to_send, address)
+  
 
-msgFromServer = "Hello UDP Client"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("PORT_NUMBER", help="Port number")
+    args = parser.parse_args()
 
-bytesToSend = str.encode(msgFromServer)
-
-
-# Create a datagram socket
-
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-
-# Bind to address and ip
-
-UDPServerSocket.bind((localIP, localPort))
-
-
-print("UDP server up and listening")
-
-
-# Listen for incoming datagrams
-
-while(True):
-
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
-    message = bytesAddressPair[0]
-
-    address = bytesAddressPair[1]
-
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP = "Client IP Address:{}".format(address)
-
-    print(clientMsg)
-    print(clientIP)
-
-    # Sending a reply to client
-
-    UDPServerSocket.sendto(bytesToSend, address)
+    main(args.PORT_NUMBER)
