8b9afaa1849c67abd44679eb36c7214b120a812a
Son Chau <soncqb@gmail.com>
2021-04-12 00:15:09 -0500

    Create main.py

---
+++ link_state/main.py
@@ -0,0 +1,102 @@
+#############################################
+# Name: Son Chau (sonchau@my.unt.edu)
+# Course: CSE 5580 Networking Spring 2021
+# Date: 04/22/2021
+# Description: implement the OSPF link-state (LS)
+# algorithm for a given set of nodes identified
+# by a lower-case letter (a – z). The output of
+# the program will be the shortest path cost and
+# path taken for each node, when given a source node.
+#############################################
+import sys
+
+
+def main():
+    value = int(input("Enter the number of routers: "))
+    value = input("Enter filename with cost matrix values: ")
+    value = input("Enter character representation of first node: ")
+    value = input("Enter the source router: ")
+    print(f'You entered {value}')
+
+    # Driver program
+    g = Graph(9)
+    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+               [4, 0, 8, 0, 0, 0, 0, 11, 0],
+               [0, 8, 0, 7, 0, 4, 0, 0, 2],
+               [0, 0, 7, 0, 9, 14, 0, 0, 0],
+               [0, 0, 0, 9, 0, 10, 0, 0, 0],
+               [0, 0, 4, 14, 10, 0, 2, 0, 0],
+               [0, 0, 0, 0, 0, 2, 0, 1, 6],
+               [8, 11, 0, 0, 0, 0, 1, 0, 7],
+               [0, 0, 2, 0, 0, 0, 6, 7, 0]
+               ]
+
+    g.dijkstra(0)
+
+
+if __name__ == "__main__":
+    main()
+
+
+class Graph():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def printSolution(self, dist):
+        print("Vertex tDistance from Source")
+        for node in range(self.V):
+            print(node, "t", dist[node])
+
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minDistance(self, dist, sptSet):
+
+        # Initilaize minimum distance for next node
+        min = sys.maxsize
+
+        # Search not nearest vertex not in the
+        # shortest path tree
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+
+        return min_index
+
+    # Funtion that implements Dijkstra's single source
+    # shortest path algorithm for a graph represented
+    # using adjacency matrix representation
+    def dijkstra(self, src):
+
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+
+        for cout in range(self.V):
+
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # u is always equal to src in first iteration
+            u = self.minDistance(dist, sptSet)
+
+            # Put the minimum distance vertex in the
+            # shotest path tree
+            sptSet[u] = True
+
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shotest path tree
+            for v in range(self.V):
+                if (
+                    self.graph[u][v] > 0 and
+                    sptSet[v] == False and
+                    dist[v] > dist[u] + self.graph[u][v]
+                    ):
+                    dist[v] = dist[u] + self.graph[u][v]
+
+        self.printSolution(dist)
