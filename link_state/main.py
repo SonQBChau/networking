4e7fc22a3c0eb7ff167effaa28f1c7621f163803
Son Chau <soncqb@gmail.com>
2021-04-17 13:21:01 -0500

    Update main.py

--- link_state/main.py
+++ link_state/main.py
@@ -8,222 +8,82 @@
 # the program will be the shortest path cost and
 # path taken for each node, when given a source node.
 #############################################
-import sys
+
 from pathlib import Path
 
-from collections import defaultdict
-  
-
-
+###########################################
+# FIND THE INDEX WITH THE MINIMUM DISTANCE
+###########################################
 def minDistance(dist,queue):
-    # Initialize min value and min_index as -1
     minimum = float("Inf")
     min_index = -1
-        
-    # from the dist array,pick one which
-    # has min value and is till in queue
     for i in range(len(dist)):
         if dist[i] < minimum and i in queue:
             minimum = dist[i]
             min_index = i
     return min_index
 
+##########################################################
+# BACKTRACK FROM END-NODE TO START-NODE TO GET TRAVEL PATH 
+##########################################################
 def getPath(parent, j, result=''):
-    src = ord('u')
-    node = chr(src+j)
-    result += node
-    #Base Case : If j is source
+    result += str(j)
     if parent[j] == -1 : 
         return result
     return getPath(parent, parent[j], result=result)
 
-def printSolution(dist, parent):
-    src = ord('u')
-    cha = chr(src)
+#################################
+# PRINT PATH COST AND ROUTE TAKEN
+#################################
+def printSolution(dist, parent, representation_node, source_router):
+    representation_ascii = ord(representation_node)
     for i in range(0, len(dist)):
-        node = chr(src+i)
+        dest_node = chr(representation_ascii + i)
         reserve_path = ''
-        print('{} ==> {}:'.format(cha, node))
+        print('{} ==> {}:'.format(source_router, dest_node))
         print('path cost: {}'.format(dist[i]))
         path = getPath(parent,i)
-        
-        # Iterate over the string
-        for element in reversed(path):
-            reserve_path += element + ' --> '
-        reserve_path = reserve_path[:-5]
+        # since path is in reversed order, we need to switch it back
+        for node in reversed(path):
+            node_name = chr(int(node) + representation_ascii)
+            reserve_path += node_name + ' --> '
+        reserve_path = reserve_path[:-5] # remove extra string at the end
         print('path taken: {}'.format(reserve_path))
 
-def dijkstra(graph, src):
-  
+#########################################
+# RUN DIJKSTRA'S SHORTEST PATH ALGORITHM
+#########################################
+def runLinkState(graph, src):
         row = len(graph)
         col = len(graph[0])
-  
-        # The output array. dist[i] will hold
-        # the shortest distance from src to i
-        # Initialize all distances as INFINITE 
         dist = [float("Inf")] * row
-  
-        #Parent array to store 
-        # shortest path tree
         parent = [-1] * row
-  
-        # Distance of source vertex 
-        # from itself is always 0
         dist[src] = 0
-      
-        # Add all vertices in queue
         queue = []
         for i in range(row):
             queue.append(i)
-              
-        #Find shortest path for all vertices
         while queue:
-  
-            # Pick the minimum dist vertex 
-            # from the set of vertices
-            # still in queue
-            u = minDistance(dist,queue) 
-  
-            # remove min element     
+            u = minDistance(dist,queue)  
             queue.remove(u)
-  
-            # Update dist value and parent 
-            # index of the adjacent vertices of
-            # the picked vertex. Consider only 
-            # those vertices which are still in
-            # queue
             for i in range(col):
-                '''Update dist[i] only if it is in queue, there is
-                an edge from u to i, and total weight of path from
-                src to i through u is smaller than current value of
-                dist[i]'''
                 if graph[u][i] and i in queue:
                     if (dist[u] + graph[u][i] < dist[i]) and graph[u][i] > 0:
                         dist[i] = dist[u] + graph[u][i]
                         parent[i] = u
-  
-  
-        # print the constructed distance array
-        # printSolution(dist,parent)
         return (dist,parent)
-'''      
-#Class to represent a graph
-class Graph:
-  
-    # A utility function to find the 
-    # vertex with minimum dist value, from
-    # the set of vertices still in queue
-    def minDistance(self,dist,queue):
-        # Initialize min value and min_index as -1
-        minimum = float("Inf")
-        min_index = -1
-          
-        # from the dist array,pick one which
-        # has min value and is till in queue
-        for i in range(len(dist)):
-            if dist[i] < minimum and i in queue:
-                minimum = dist[i]
-                min_index = i
-        return min_index
-  
-  
-    # Function to print shortest path
-    # from source to j
-    # using parent array
-    def printPath(self, parent, j):
-        src = ord('u')
-        node = chr(src+j)
-        #Base Case : If j is source
-        if parent[j] == -1 : 
-            print (node, end=', '),
-            return
-        self.printPath(parent , parent[j])
-        print (node, end=', '),
 
-      # Function to print shortest path
-    # from source to j
-    # using parent array 
-    def getPath(self, parent, j, result=''):
-        src = ord('u')
-        node = chr(src+j)
-        result += node
-        #Base Case : If j is source
-        if parent[j] == -1 : 
-            return result
-        return self.getPath(parent, parent[j], result=result)
- 
-  
-    # A utility function to print
-    # the constructed distance
-    # array
-    def printSolution(self, dist, parent):
-        src = ord('u')
-        cha = chr(src)
-        for i in range(0, len(dist)):
-            node = chr(src+i)
-            reserve_path = ''
-            print('{} ==> {}:'.format(cha, node))
-            print('path cost: {}'.format(dist[i]))
-            path = self.getPath(parent,i)
-            
-            # Iterate over the string
-            for element in reversed(path):
-                reserve_path += element + ' --> '
-            reserve_path = reserve_path[:-5]
-            print('path taken: {}'.format(reserve_path))
-            # self.printPath(parent,i)
-  
-  
-
-    def dijkstra(self, graph, src):
-  
-        row = len(graph)
-        col = len(graph[0])
-  
-        # The output array. dist[i] will hold
-        # the shortest distance from src to i
-        # Initialize all distances as INFINITE 
-        dist = [float("Inf")] * row
-  
-        #Parent array to store 
-        # shortest path tree
-        parent = [-1] * row
-  
-        # Distance of source vertex 
-        # from itself is always 0
-        dist[src] = 0
-      
-        # Add all vertices in queue
-        queue = []
-        for i in range(row):
-            queue.append(i)
-              
-        #Find shortest path for all vertices
-        while queue:
-  
-            # Pick the minimum dist vertex 
-            # from the set of vertices
-            # still in queue
-            u = self.minDistance(dist,queue) 
-  
-            # remove min element     
-            queue.remove(u)
-  
-            # Update dist value and parent 
-            # index of the adjacent vertices of
-            # the picked vertex. Consider only 
-            # those vertices which are still in
-            # queue
-            for i in range(col):
-                if graph[u][i] and i in queue:
-                    if (dist[u] + graph[u][i] < dist[i]) and graph[u][i] > 0:
-                        dist[i] = dist[u] + graph[u][i]
-                        parent[i] = u
-  
-  
-        # print the constructed distance array
-        self.printSolution(dist,parent)
-'''  
+#######################################
+# READ FILE INPUT AND RETURN A 2D ARRAY
+#######################################
+def readFile(name):
+    matrix = []
+    script_location = Path(__file__).absolute().parent
+    file_location = script_location / name
+    f = open(file_location, "r")
+    for line in f.readlines():
+            matrix.append([int(d) for d in line.split(' ')])
+    f.close()
+    return matrix
 
 
 def main():
@@ -235,22 +95,15 @@ def main():
     # source_router = input("Enter the source router: ")
     #need error checking
 
-    matrix = []
-    # read matrix from file
-    script_location = Path(__file__).absolute().parent
-    file_location = script_location / 'text.txt'
-    f = open(file_location, "r")
-    for line in f.readlines():
-            matrix.append([int(d) for d in line.split(' ')])
-    print(matrix)
-    f.close()
+    router_num = 0
+    input_name = 'text.txt'
+    representation_node = 'u'
+    source_router = 'v'
+    source_index = ord(source_router) - ord(representation_node)
 
-    # Driver program
-    # g = Graph()
-    # graph = matrix
-    # g.dijkstra(graph,0)
-    dist, parent = dijkstra(matrix,0)
-    printSolution(dist,parent)
+    matrix = readFile(input_name)
+    dist, parent = runLinkState(matrix,source_index)
+    printSolution(dist,parent, representation_node, source_router)
 
 
 if __name__ == "__main__":
