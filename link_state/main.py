37d840f724545f5ec3f2c858e1e79731f1aec80c
Son Chau <soncqb@gmail.com>
2021-04-17 11:32:43 -0500

    refactor using function calls

--- link_state/main.py
+++ link_state/main.py
@@ -13,6 +13,100 @@ from pathlib import Path
 
 from collections import defaultdict
   
+
+
+def minDistance(dist,queue):
+    # Initialize min value and min_index as -1
+    minimum = float("Inf")
+    min_index = -1
+        
+    # from the dist array,pick one which
+    # has min value and is till in queue
+    for i in range(len(dist)):
+        if dist[i] < minimum and i in queue:
+            minimum = dist[i]
+            min_index = i
+    return min_index
+
+def getPath(parent, j, result=''):
+    src = ord('u')
+    node = chr(src+j)
+    result += node
+    #Base Case : If j is source
+    if parent[j] == -1 : 
+        return result
+    return getPath(parent, parent[j], result=result)
+
+def printSolution(dist, parent):
+    src = ord('u')
+    cha = chr(src)
+    for i in range(0, len(dist)):
+        node = chr(src+i)
+        reserve_path = ''
+        print('{} ==> {}:'.format(cha, node))
+        print('path cost: {}'.format(dist[i]))
+        path = getPath(parent,i)
+        
+        # Iterate over the string
+        for element in reversed(path):
+            reserve_path += element + ' --> '
+        reserve_path = reserve_path[:-5]
+        print('path taken: {}'.format(reserve_path))
+
+def dijkstra(graph, src):
+  
+        row = len(graph)
+        col = len(graph[0])
+  
+        # The output array. dist[i] will hold
+        # the shortest distance from src to i
+        # Initialize all distances as INFINITE 
+        dist = [float("Inf")] * row
+  
+        #Parent array to store 
+        # shortest path tree
+        parent = [-1] * row
+  
+        # Distance of source vertex 
+        # from itself is always 0
+        dist[src] = 0
+      
+        # Add all vertices in queue
+        queue = []
+        for i in range(row):
+            queue.append(i)
+              
+        #Find shortest path for all vertices
+        while queue:
+  
+            # Pick the minimum dist vertex 
+            # from the set of vertices
+            # still in queue
+            u = minDistance(dist,queue) 
+  
+            # remove min element     
+            queue.remove(u)
+  
+            # Update dist value and parent 
+            # index of the adjacent vertices of
+            # the picked vertex. Consider only 
+            # those vertices which are still in
+            # queue
+            for i in range(col):
+                '''Update dist[i] only if it is in queue, there is
+                an edge from u to i, and total weight of path from
+                src to i through u is smaller than current value of
+                dist[i]'''
+                if graph[u][i] and i in queue:
+                    if (dist[u] + graph[u][i] < dist[i]) and graph[u][i] > 0:
+                        dist[i] = dist[u] + graph[u][i]
+                        parent[i] = u
+  
+  
+        # print the constructed distance array
+        # printSolution(dist,parent)
+        return (dist,parent)
+'''      
 #Class to represent a graph
 class Graph:
   
@@ -80,9 +174,7 @@ class Graph:
             # self.printPath(parent,i)
   
   
-    '''Function that implements Dijkstra's single source shortest path
-    algorithm for a graph represented using adjacency matrix
-    representation'''
+
     def dijkstra(self, graph, src):
   
         row = len(graph)
@@ -123,10 +215,6 @@ class Graph:
             # those vertices which are still in
             # queue
             for i in range(col):
-                '''Update dist[i] only if it is in queue, there is
-                an edge from u to i, and total weight of path from
-                src to i through u is smaller than current value of
-                dist[i]'''
                 if graph[u][i] and i in queue:
                     if (dist[u] + graph[u][i] < dist[i]) and graph[u][i] > 0:
                         dist[i] = dist[u] + graph[u][i]
@@ -135,7 +223,7 @@ class Graph:
   
         # print the constructed distance array
         self.printSolution(dist,parent)
-  
+'''  
 
 
 def main():
@@ -158,9 +246,11 @@ def main():
     f.close()
 
     # Driver program
-    g = Graph()
-    graph = matrix
-    g.dijkstra(graph,0)
+    # g = Graph()
+    # graph = matrix
+    # g.dijkstra(graph,0)
+    dist, parent = dijkstra(matrix,0)
+    printSolution(dist,parent)
 
 
 if __name__ == "__main__":
